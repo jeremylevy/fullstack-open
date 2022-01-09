e9d9de40469c60c26e0fafec7cd200a5469a2f69
Jeremy Levy <jje.levy@gmail.com>
2022-01-09 10:27:59 +0100

    Finish 2.16: Phonebook step8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personsService from './services/persons'
 
 const Filter = ({ onChange, value }) => (
   <div>
@@ -38,9 +38,9 @@ const App = () => {
   const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    personsService
+      .getAll()
+      .then(persons => setPersons(persons))
   }, [])
 
   const personAlreadyExists = (personToCheck) => {
@@ -80,12 +80,12 @@ const App = () => {
       return
     }
 
-    axios
-      .post('http://localhost:3001/persons', newPerson)
-      .then(response => {
+    personsService
+      .create(newPerson)
+      .then(createdPerson => {
         const updatedPersons = [
           ...persons,
-          response.data
+          createdPerson
         ]
     
         setPersons(updatedPersons)
