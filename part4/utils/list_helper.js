a40b48224541640fdae013ec5b80795045f63cf2
Jeremy Levy <jje.levy@gmail.com>
2022-01-11 12:29:48 +0100

    Finish 4.5*: helper functions and unit tests, step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,7 +4,10 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum += blog.likes, 0)
 
+const favoriteBlog = (blogs) => [...blogs].sort((blog1, blog2) => blog2.likes - blog1.likes)[0]
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
