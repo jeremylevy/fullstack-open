683b509484f656c2a364bf99db5c12d471b8b1ae
Jeremy Levy <jje.levy@gmail.com>
2022-01-12 19:10:29 +0100

    Finish 4.9*: Blog list tests, step2

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -24,6 +24,17 @@ test('blogs are returned as json', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('returned blogs have an \'id\' property', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+
+  expect(blogs.length).toBeGreaterThan(0)
+
+  for (let blog of blogs) {
+    expect(blog.id).toBeDefined()
+  }
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
