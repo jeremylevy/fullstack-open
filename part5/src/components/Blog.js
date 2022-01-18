ce670321ba82914869b28388d0c4d66f7b2725d9
Jeremy Levy <jje.levy@gmail.com>
2022-01-18 17:24:28 +0100

    Finish 5.7* Blog list frontend, step7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,48 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleExpandedView = (event) => {
+    event.preventDefault()
+    setIsExpanded(!isExpanded)
+  }
+
+  const collapsedView = () => (
+    <div>
+      <p>
+        {blog.title}
+        &nbsp;
+        <button onClick={toggleExpandedView}>view</button>
+      </p>
+    </div>
+  )
+
+  const expandedView = () => (
+    <div>
+      <p>
+        {blog.title}
+        &nbsp;
+        <button onClick={toggleExpandedView}>hide</button>
+      </p>
+      
+      <p>{blog.url}</p>
+      
+      <p>
+        likes {blog.likes}
+        &nbsp;
+        <button>like</button>
+      </p>
+      
+      <p>{blog.author}</p>
+    </div>
+  )
+
+  return (
+    <div className="blog">
+      { isExpanded ? expandedView() : collapsedView() }
+    </div>
+  )
+}
 
 export default Blog
