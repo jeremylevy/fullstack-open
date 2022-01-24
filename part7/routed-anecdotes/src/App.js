b5396fd0f4bb13d5f9188e39a247c52b913dbb17
Jeremy Levy <jje.levy@gmail.com>
2022-01-24 18:45:15 +0100

    Finish 7.5: anecdotes and hooks step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -81,6 +81,14 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+
+    contentField.reset()
+    authorField.reset()
+    infoField.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -98,6 +106,7 @@ const CreateNew = (props) => {
           <input name='info' { ...infoField } />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
