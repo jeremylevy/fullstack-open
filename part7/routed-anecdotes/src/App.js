18e3b4bbd3b6c7d43d520a711972202b4c6d5f26
Jeremy Levy <jje.levy@gmail.com>
2022-01-24 18:02:14 +0100

    Finish 7.2: routed anecdotes, step2

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
-  Switch, Route, Link
+  Switch, Route, Link, useRouteMatch
 } from "react-router-dom"
 
 const Menu = () => {
@@ -17,11 +17,27 @@ const Menu = () => {
   )
 }
 
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+  </div>
+)
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {
+        anecdotes.map(anecdote =>
+          <li key={anecdote.id}>
+            <Link to={"/anecdotes/" + anecdote.id}>
+              {anecdote.content}
+            </Link>
+          </li>
+        )
+      }
     </ul>
   </div>
 )
@@ -78,7 +94,7 @@ const CreateNew = (props) => {
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
         </div>
         <button>create</button>
       </form>
@@ -126,28 +142,38 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useRouteMatch('/anecdotes/:id')
+  const anecdote = match ? anecdoteById(match.params.id) : null
+
   return (
     <div>
-      <Router>
-        <div>
-          <h1>Software anecdotes</h1>
-          <Menu />
-          <Switch>
-            <Route path="/create">
-              <CreateNew addNew={addNew} />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/">
-              <AnecdoteList anecdotes={anecdotes} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-      <Footer />
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Switch>
+        <Route path="/create">
+          <CreateNew addNew={addNew} />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/anecdotes/:id">
+          <Anecdote anecdote={anecdote} />
+        </Route>
+        <Route path="/">
+          <AnecdoteList anecdotes={anecdotes} />
+        </Route>
+      </Switch>
     </div>
   )
 }
 
-export default App;
+const RoutedApp = () => (
+  <div>
+    <Router>
+      <App />
+    </Router>
+    <Footer />
+  </div>
+)
+
+export default RoutedApp;
